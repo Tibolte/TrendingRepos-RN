5700ce8bd6e38905d8f523202af5554a2846fd8a
Thibault Guegan <thibault.guegan@ladbrokes.com.au>
2018-05-16 11:08:26 +1000

    Add missing manifest and add setup react-native-debugger

--- app/store/configure-store.js
+++ app/store/configure-store.js
@@ -1,12 +1,14 @@
 'use strict';
 
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore (initState) {
-  const store = createStoreWithMiddleware(rootReducer, initState);
+  const store = createStore(rootReducer, initState, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ));
   return store;
 }
